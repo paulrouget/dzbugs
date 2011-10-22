5d9ed075bc8e0bf7d452bf0a42efd753b28960ef
paul <Paul Rouget paul@mozilla.com>
2011-10-23 01:43:32 +0200

    a lot of new stuff

--- dzbugs.js
+++ dzbugs.js
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
-var tty = require('tty');
 
-require("coffee-script")
-var T = require("node-term-ui").TermUI;
+/* Includes */
+
+require("coffee-script");
+var notify = require("./lib/notifications.js");
 var bz = require("bz")
 var BugStore = require("./lib/bugstore.js").BugStore;
 var printers = require("./lib/printers.js");
-var f = require("./lib/format.js").formater;
+var formater = require("./lib/format.js").formater;
+var layout = require("./lib/layout.js").layout;
 
+console.log(layout);
 
+/* Init store */
+
+var store = new BugStore(layout);
+layout.init(store, formater);
+store.load("/tmp/dzbugs.data");
+
+return;
+
+//var bugsIds = [669658, 653545, 663778, 672003, 672006, 683873];
 
 var bugzilla = bz.createClient();
-var store = new BugStore();
-
-var bugsIds = [669658, 653545, 663778, 672003, 672006, 683873];
-//var bugsIds = [653545];
-
-T.clear();
-var dim = tty.getWindowSize(process);
-var row = dim[0];
-var col = dim[1];
-
 fetchBugsFromBugzilla(bugsIds, function() {
+  store.save("/tmp/dzbugs.data");
+  return;
   var line = 0;
   for (var i in bugsIds) {
     var bug = store.getBug(bugsIds[i]);
