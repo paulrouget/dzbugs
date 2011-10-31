6139375305558e598202c96de9dbd589dbf043ab
Paul Rouget <paul.rouget@gmail.com>
2011-10-31 14:23:09 +0100

    bug panel

--- etc/conf.js
+++ etc/conf.js
@@ -1,4 +1,7 @@
 exports.email = "paul@mozilla.com";
+exports.defaultFilter  = "notfixed";
+exports.defaultSorter = "priority";
+exports.bugPanelOffset = 5;
 exports.bugzilla = {
   apiUrl: "https://api-dev.bugzilla.mozilla.org/latest",
   //FIXME: username and password
--- etc/format.js
+++ etc/format.js
@@ -18,26 +18,35 @@ var formater = {
     if (current) str += bg(235);
     str += bug.tags.indexOf("important") != -1 ? "!" : " ";
 
+    var priority = bug.bz.priority;
+    if (priority == "P1")
+      priority = fg(196) + "#";
+    else if (priority == "P2")
+      priority = fg(208) + "#";
+    else if (priority == "P3")
+      priority = fg(190) + "#";
+    else
+      priority = " ";
+
+    var summary = bug.alias ? bug.alias + "*" : b.summary;
+
     if (b.status == "RESOLVED" || b.status == "VERIFIED") {
       str += fg(238);
-      str += fw(idx + 1, 5, " ", true) + " " + (current? ">" : " ");
-      str += '[' + fw(b.id, 6, " ", true) + '] ';
-      str += fw(b.status, 8) + " ";
-      summary = bug.alias ? bug.alias + "*" : b.summary;
+      str += fw(idx + 1, 5, " ", true) + " " + (current? "> " : "  ");
+      str += '  [' + fw(b.id, 6, " ", true) + '] ';
+      str += fw(b.status, 3) + " ";
       str += " " + summary;
     } else {
-      str += fw(idx + 1, 5, " ", true) + " " + (current? ">" : " ");
-      str += fg(28) + '[' + fw(b.id, 6, " ", true) + '] ';
-      str += fg(220) + fw(b.status, 8) + " " + fgr();
-      summary = bug.alias ? bug.alias + "*" : b.summary;
+      str += fw(idx + 1, 5, " ", true) + " " + (current? "> " : "  ");
+      str += priority + fg(28) + '[' + fw(b.id, 6, " ", true) + '] ';
+      str += fg(220) + fw(b.status, 3) + " " + fgr();
       str += " " + summary.replace("[", fg(199) + "[", "g").replace("]", "]" + fgr(), "g"); //FIXME: works just once
     }
 
     return str;
   },
   bugEnd: function(bug, current, idx) {
-    return JSON.stringify(bug.bz.cc);
-    //return fg(288) + bug.tags.join(" ") + " " + fg(239) + " http://bugzil.la/" + bug.id;
+    return fg(288) + bug.tags.join(" ") + " " + fg(239) + " http://bugzil.la/" + bug.id;
   },
   statusStart: function() {
     return bg(244) + fg(0);
@@ -45,7 +54,37 @@ var formater = {
   statusEnd: function() {
     var s = DZ.store;
     return "[" + s.getFilterName() + "] [" + s.getSorterName() + "]" + (s.reverse ? "@ ": " ") + s.getCount() + "/" + s.getCountAll();
-  }
+  },
+
+
+  fullBug: function(dzBug, bzBug, width) {
+    var txt = "", nl = "\n";
+    var bz = bzBug;
+
+    var separator = "";
+    for (var i = 1; i < width; i++) separator += "_";
+    separator += nl;
+
+    txt += "Bug " + bz.id + " - " + bz.summary + nl;
+    txt += "Status: " + bz.status + nl;
+    txt += "_" + separator;
+
+    var comments = bz.comments;
+
+    var idx = 0;
+    comments.forEach(function(c) {
+      var prefix = idx == 0 ? bg(54) + " " + bgr() + " " : "  ";
+      txt += prefix + "comment #" + idx + " by " + fg(198) + c.creator.name + nl;
+      var body = c.text.replace(/\n>/g, "\n" + fg(239) + ">" );
+      body = body.replace(/\n/g, fgr() + nl + prefix);
+      txt += prefix + nl + prefix + body + nl;
+      prefix = idx == 0 ? bg(54) + "_" + bgr() : "_";
+      txt += prefix + separator;
+      idx++;
+    });
+
+    return txt;
+  },
 }
 
 module.exports = formater;
